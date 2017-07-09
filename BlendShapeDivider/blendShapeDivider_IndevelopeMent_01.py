58b6bf3ad4e70e1ca5949e5e5317f2d6f5419756
Jingtian Li <jingtianli.animation@gmail.com>
2017-07-09 02:38:49 -0500

    controller feature implemented

--- BlendShapeDivider/blendShapeDivider_IndevelopeMent_01.py
+++ BlendShapeDivider/blendShapeDivider_IndevelopeMent_01.py
@@ -1,9 +1,14 @@
 #BlendShape Divider
 """
-Original reference: BlendTapper 5.0
 Developed by Jingtian Li
 
 Ver 1.0
+
+features:
+    > support both polygon and ribbon like nurbs that are not closed
+    > controller creation included
+    > TODO smoothness of shapes
+Original reference: BlendTapper 5.0
 """
 
 #Libraries
@@ -22,41 +27,51 @@ if (mc.window(BlendShapeDividerWindowID, q = True, exists = True)):
     mc.deleteUI(BlendShapeDividerWindowID)
 mc.window(BlendShapeDividerWindowID, title = "BlendShape Divider", rtf = True)
 BlendShapeWindowMasterLayout = mc.frameLayout(lv = 0, bv = False)
-mc.rowColumnLayout(nc = 2)
+
+#Number of division section
+mc.rowColumnLayout(nc = 1)
 divisitonintField = mc.intSliderGrp(l = "Number of Divisions:   ", f = True, s = True, min = 2, max = 20, v= 2)
+mc.textFieldGrp("ControllerTextFieldID", l = "Namebase:            ")
+
+# create controller section
 mc.setParent(BlendShapeWindowMasterLayout)
 createControllerPortionlayout = mc.rowColumnLayout(nc = 5)
-createCtrlCB = mc.checkBox(l = "Create Controller",v = True,onc = "createCtrlCBOnCmd()", ofc = "createCtrlCBOffCmd()")
+createCtrlCB = mc.checkBox(l = "Controller",v = True,onc = "createCtrlCBOnCmd()", ofc = "createCtrlCBOffCmd()")
 controllerStyleRC = mc.radioCollection("ControllerSelectionID")
 mc.radioButton("Slider",l = "Sliders", sl = True)
 mc.radioButton("Circle",l = "Circles")
-mc.text("ControllerTextID",l = "      Namebase: ")
-mc.textField("ControllerTextFieldID")
+mc.setParent(createControllerPortionlayout)
+mc.checkBox("PosDirection",l = "Positive Direction", v = True)
+mc.setParent(BlendShapeWindowMasterLayout)
+hintLayout = mc.columnLayout()
+mc.text("hintText",l = "to use existing controllers, add select the controllers")
+
+#specifiy base shape section
 mc.setParent(BlendShapeWindowMasterLayout)
 SpecifyBaseShapeLayout = mc.rowColumnLayout(nc = 5)
 SpecifyBaseShapeCB = mc.checkBox(l = "Specify BaseShape", v = True, onc = "SpecifyBaseShapeCBOnCmd()", ofc = "SpecifyBaseShapeCBOffCmd()")
 BasicShapeTextField = mc.textField("BasicShapeTextFieldID", w = 250, ed = False, vis = True)
 assingBaseShapeBtn = mc.button("assingBaseShapeBtnID", l = "<<<", vis = True, c = "assingBaseShapeBtnCmd()")
+
+#specify destination blendshape section
+mc.setParent(BlendShapeWindowMasterLayout)
+mc.rowColumnLayout(nc = 3)
+mc.text(l = "Destination blendShape: ")
+DestinationBlspTextField = mc.textField(w = 237, ed = False)
+AssignDestinationBtn = mc.button(l = "<<<", c = "AssignDestinationBtnCmd()")
+
 mc.setParent(BlendShapeWindowMasterLayout)
 ApplyButtonSectionLayout = mc.rowColumnLayout(nc = 3)
-mc.button(l = "Create Plane", w = 135, c = "CreatePlaneBtnCmd()")
-mc.button(l = "Make blendshapes",w = 135, c = "MakeBlspBtnCmd()")
-mc.button(l = "Cancel", w = 135,c = "CancelBtnCmd()")
+mc.button(l = "Create Plane", w = 200, c = "CreatePlaneBtnCmd()")
+mc.button(l = "Make blendshapes",w = 200, c = "MakeBlspBtnCmd()")
 mc.showWindow(BlendShapeDividerWindowID)
 
 ########################################################################
 ####                            Methods                             ####
 ########################################################################
 
-#simple clamp fucntion:
-def clamp(min, max, value):
-    if value < min:
-        value = min
-    elif value > max:
-        value = max
-    return value
 
-#smooth step using maya.mel.eval
+#smooth step using maya.mel.eval(smoothstep)
 def smoothStep(min, max, value):
     minString = str(min)
     maxString = str(max)
@@ -64,18 +79,20 @@ def smoothStep(min, max, value):
     percentage = mel.eval("smoothstep(" + minString + "," + maxString + "," + valueString + ")" )
     return percentage
 
-#print the x, y, z value of the om.MVector passed in
+#print the x, y, z value of the om.MVector passed in this fuction is for debug purpose
 def printMVector(VectorToPrint):
     print VectorToPrint.x, VectorToPrint.y, VectorToPrint.z
 
+#print information gathered from UI for debug purpose only
 def printInfo():
     #Gather Info
     NumberOfDivision = mc.intSliderGrp(divisitonintField, q = True, v =True)
     createCtroller = mc.checkBox(createCtrlCB, q = True, v = True)
     controllerType = mc.radioCollection("ControllerSelectionID", q = True, sl = True)
-    NameBase = mc.textField("ControllerTextFieldID", q = True, tx = True)
+    NameBase = mc.textFieldGrp("ControllerTextFieldID", q = True, tx = True)
     specifyBaseShape = mc.checkBox(SpecifyBaseShapeCB, q = True, v = True)
     BaseShape = mc.textField("BasicShapeTextFieldID", q = True, tx = True)
+    positiveDirection = mc.checkBox("PosDirection", q = True, v = True)
     
     print "Number of Division is:", NumberOfDivision
     print "Should create controller:", createCtroller
@@ -88,13 +105,18 @@ def printInfo():
 def createCtrlCBOnCmd():
     mc.radioButton("Slider",e = True, vis = True)
     mc.radioButton("Circle",e = True, vis = True)
-    mc.text("ControllerTextID", e = True, vis = True)
-    mc.textField("ControllerTextFieldID", e = True, vis = True)
+    mc.checkBox("PosDirection",e = True, vis = True)
+    
+    mc.setParent(hintLayout)
+    mc.text("hintText",l = "to use existing controllers, add select the controllers")
+    
 def createCtrlCBOffCmd():
     mc.radioButton("Slider",e = True, vis = False)
     mc.radioButton("Circle",e = True, vis = False)
-    mc.text("ControllerTextID", e = True, vis = False)
-    mc.textField("ControllerTextFieldID", e = True, vis = False)
+    mc.checkBox("PosDirection",e = True, vis = False)
+    
+    if mc.text("hintText", q = True, exists = True):
+        mc.deleteUI("hintText")
     
 def SpecifyBaseShapeCBOnCmd():
     mc.textField("BasicShapeTextFieldID", e = True, vis = True)
@@ -109,19 +131,24 @@ def assingBaseShapeBtnCmd():
     shapeOfSelection = mc.listRelatives(selection, s = True)[0]
     mc.textField("BasicShapeTextFieldID", e = True, tx = shapeOfSelection)
 
+def AssignDestinationBtnCmd():
+    sel = mc.ls(sl = True)[0]
+    if mc.objectType(sel, i="blendShape"):
+        mc.textField(DestinationBlspTextField, e = True, tx = sel)
+    else:
+        mc.error("not a blendShape")
+    
 
-def CancelBtnCmd():
-    if (mc.window(BlendShapeDividerWindowID, q = True, exists = True)):
-        mc.deleteUI(BlendShapeDividerWindowID)
     
 def MakeBlspBtnCmd():
-
     #Gather Info from UI
     createCtroller = mc.checkBox(createCtrlCB, q = True, v = True)
     controllerType = mc.radioCollection("ControllerSelectionID", q = True, sl = True)
-    NameBase = mc.textField("ControllerTextFieldID", q = True, tx = True)
+    NameBase = mc.textFieldGrp("ControllerTextFieldID", q = True, tx = True)
     specifyBaseShape = mc.checkBox(SpecifyBaseShapeCB, q = True, v = True)
     BaseShapeFiledText = mc.textField("BasicShapeTextFieldID", q = True, tx = True)
+    DestinationBlsp = mc.textField(DestinationBlspTextField, q = True, tx = True)
+    positiveDirection = mc.checkBox("PosDirection", q = True, v = True)
     
     #find targetShape: 
     sel = mc.ls(sl = True)[0]
@@ -146,6 +173,17 @@ def MakeBlspBtnCmd():
     #find number of divistions specified:
     NumberOfDivisions = len(divisionPlaneList)
 
+    #find if there are controllers selected:
+    controllerList = []
+    fullSelection = mc.ls(sl = True)
+    if len(fullSelection)>1:
+        print "controller seletion detected"
+        for item in fullSelection:
+            if item == sel:
+                continue
+            else:
+                controllerList.append(item)
+        
     #get base and target position and their vertex List:
     BasePosition = getTranslateNodePostion(BaseShape)
     TargetPosition = getTranslateNodePostion(TargetShape)
@@ -157,44 +195,135 @@ def MakeBlspBtnCmd():
     boundingBox = boundingBoxInfo[0]
     bIsBoundingBoxValid = boundingBoxInfo[1]
 
-    print "base shape is: " + BaseShape
-    print "Target Shape is: " + TargetShape
-    print "Division Planes are:", divisionPlaneList 
-    print "Number of Divisiion is:", str(NumberOfDivisions)
-    
     newShapeList = []
     lastShape = TargetShape
     for currentDiv in range(0, NumberOfDivisions):
         blendedVerts = CalculateBlend(BaseVertList,TargetVertList, BasePosition, TargetPosition, boundingBox, NumberOfDivisions, currentDiv,divisionPlaneList)
         newShapeList.append(newBlend(blendedVerts, BaseShape, TargetShape, NumberOfDivisions, currentDiv, lastShape))
+    mc.delete(divisionPlaneList)
     
+    #Assign BlendShape:
+    #figure out how many blendshapes are already on the blendshape node specified in Destination blenshape
+    blendShapeAttrList = []
+    blendShapeStartIndex = len(mc.blendShape(sel, q=True, w=1))
+    for blendShapeTarget in newShapeList:
+        newBlsp = addBlendShape(sel,blendShapeTarget, DestinationBlsp, blendShapeStartIndex)  
+        blendShapeStartIndex = blendShapeStartIndex + 1
+        blendShapeAttrList.append(newBlsp)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    #delete shape list after adding blendshape:
+    mc.delete(newShapeList)
+    #create Controller
+    if createCtroller:
+        for i in range(0, len(blendShapeAttrList)):
+            nameIndex = i + 1
+            ControllerForAttr(blendShapeAttrList[i], controllerType, positiveDirection, nameIndex, controllerList)
+            
+#create or connect specified controller for given attribute    
+def ControllerForAttr(Attr, Type, PosDir, nameIndex ,List):
+    if len(List):
+        
+        controllerToUse = List[nameIndex-1]
+        print "using selected controller:", controllerToUse
+        CtrlCurrentLimit = mc.transformLimits(controllerToUse, q = True, ty = True)
+        if PosDir:
+            mc.transformLimits(controllerToUse, ety = (1,1),ty = (CtrlCurrentLimit[0], 1))
+            mc.connectAttr(controllerToUse + ".Pos_Y", Attr) 
+        else:
+            mc.transformLimits(controllerToUse, ety = (1,1),ty = (-1, CtrlCurrentLimit[1]))
+            mc.connectAttr(controllerToUse + ".Neg_Y", Attr)
+        #figure the new limits:
+        newLimit = mc.transformLimits(controllerToUse, q = True, ty = True)
+        range = controllerToUse + "_range"
+        if mc.objExists(range):
+            minLimitCV = range + ".cv[0]"
+            maxLimitCV = range + ".cv[1]"
+            mc.xform(minLimitCV, ws = False, t = (0, newLimit[0], 0))
+            mc.xform(maxLimitCV, ws = False, t = (0, newLimit[1], 0))
+            
+    else:
+        controllerNameBase = mc.textFieldGrp("ControllerTextFieldID", q = True, tx = True)
+        controllerName = "ac_" + controllerNameBase + "_" + str(nameIndex)
+        mc.circle(n = controllerName, nr = (0,0,1), ch = False, r = 0.15)
+        
+        #hide un nessary controlls
+        mc.setAttr(controllerName + ".tx",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".tz",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".sx",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".sy",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".sz",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".rx",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".ry",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".rz",lock = True,keyable = False,channelBox = False)
+        mc.setAttr(controllerName + ".v",lock = True,keyable = False,channelBox = False)
+
+        #create clamp nodes to extract positive and nagative value:
+        clampPosNode = mc.createNode('clamp', n = controllerName + "clamp_Pos")
+        clampNegNode = mc.createNode('clamp', n = controllerName + "clamp_Neg")
+        NegReverseMD = mc.createNode('multiplyDivide', n = controllerName + "_NegReverseMD")
+        
+        mc.setAttr(clampPosNode + ".maxG", 1)
+        mc.setAttr(clampNegNode + ".minG", -1)
+        mc.setAttr(NegReverseMD + ".input2X", -1)
+        mc.setAttr(NegReverseMD + ".input2Y", -1)
+        mc.setAttr(NegReverseMD + ".input2Z", -1)
+        mc.connectAttr(controllerName + ".translate",clampPosNode + ".input") 
+        mc.connectAttr(controllerName + ".translate",clampNegNode + ".input")
+        mc.connectAttr(clampNegNode + ".output",NegReverseMD + ".input1")
+        
+        #add attribute to controller for furture useage:
+        mc.addAttr(controllerName, ln = "Pos_Y", at = "double")
+        mc.addAttr(controllerName, ln = "Neg_Y", at = "double")
+        
+        #connect clamp result to new attribute:
+        mc.connectAttr(clampPosNode + ".outputG",controllerName + ".Pos_Y")
+        mc.connectAttr(NegReverseMD + ".output.outputY",controllerName + ".Neg_Y")
+        
+        #set Limitations:
+        if PosDir:
+            mc.transformLimits(controllerName, ety = (1,1),ty = (0, 1))
+            mc.connectAttr(controllerName + ".Pos_Y", Attr)
+        else:
+            mc.transformLimits(controllerName, ety = (1,1),ty = (-1, 0))
+            mc.connectAttr(controllerName + ".Neg_Y", Attr)
+        
+        
+        #create nessary hierachy:
+        controllerGrpName = controllerName + "_grp"
+        mc.group(controllerName, n = controllerGrpName)
+        
+        if Type == "Slider":
+            rangeCurve = controllerName + "_range"
+            mc.curve(d = 1, p = [(0,0,0),(0,1,0)], n = rangeCurve)
+            mc.setAttr(rangeCurve + ".template", 1)
+            mc.parent(rangeCurve, controllerGrpName)
+        
+        mc.setAttr( controllerGrpName + ".tx", nameIndex/2.0)     
+        
+         
+#add blendShapeTarget as blendshape to DestinationModel on DestinationBlsp node and return the new attribute name on the blendshape
+def addBlendShape(DestinationModel,blendShapeTarget, DestinationBlsp, index):
+    print DestinationBlsp
+    mc.blendShape(DestinationBlsp, edit=True, t=(DestinationModel, index, blendShapeTarget, 1.0))
+    return DestinationBlsp + "." + blendShapeTarget
     
 def newBlend(blendedVerts, BaseShape, TargetShape, NumberOfDivisions, currentDiv, lastShape):
     #duplicate and rename:
-    newShape = TargetShape.replace("Shape", "") + "_" + str(currentDiv + 1)
+    NameBase = mc.textFieldGrp("ControllerTextFieldID", q = True, tx = True)
+    newShape = TargetShape.replace("Shape", "") + "_" + NameBase + "_" + str(currentDiv + 1)
     mc.duplicate(BaseShape, n = newShape)
-        
+      
     #move vertex:
     for i in range(0, len(blendedVerts)):
         #determine geo Type:
+        vertPosition = blendedVerts[i]
         if mc.objectType(BaseShape,i = "nurbsSurface"):
-            print "it is nurbs, let's deal with it latter"
+            cv = newShape + ".cv[" + str(i) + "]"
+            mc.xform(cv, ws = True, t = (vertPosition.x, vertPosition.y, vertPosition.z))
         if mc.objectType(BaseShape,i = "mesh"):   
             vert = newShape + ".vtx[" + str(i) + "]"
-            vertPosition = blendedVerts[i]
             mc.xform(vert, ws = True, t = (vertPosition.x, vertPosition.y, vertPosition.z))
-    
+    return newShape
     
     
 # get a list of vetex locations of the blendshape the division plane divisionPlaneList[currentDiv] should generate
@@ -244,6 +373,7 @@ def CalculateBlend(BaseVertList,TargetVertList, BasePosition, TargetPosition, bo
         xMaxV = planeXMax
         
         #percentage is the distance percentage form the position of the current division plane to the edge of the current division plane coverage
+        #the further the CurrentVertTranslateX from the position of the current division plane, the bigger the value will be
         #if the vertex is between the min and mid range:
         if (CurrentVertTranslateX > xMinV and CurrentVertTranslateX < xMidV):
             percentage = smoothStep(xMinV, xMidV, CurrentVertTranslateX)
@@ -254,15 +384,20 @@ def CalculateBlend(BaseVertList,TargetVertList, BasePosition, TargetPosition, bo
         #if the vertex is right on the mid:
         elif (CurrentVertTranslateX == xMidV):
             percentage = 0.0
-        #if the vertex is out of the min max range, it should have 0 percentage
+        #if the vertex is out of the min max range:
+            #>if the plane is the fisrt or the last, any vertex out side of them should have 0 percentage
+            #>if the plane is the middle ones, then the percentage will be 1
+        elif (currentDiv == 0 and CurrentVertTranslateX < xMinV) or (currentDiv == (NumberOfDivisions - 1) and CurrentVertTranslateX > xMaxV):
+            percentage = 0.0
         else:
             percentage = 1.0
+
 
         # calculate the current vertex postion of the generated blendshape by blending the base vertex position and the target vertex position by percentage
         blendedVerts.append((BaseVertList[i] * percentage) + (adjustedVert * (1-percentage)))
     return blendedVerts
 
-    
+
 def CreatePlaneBtnCmd():
     #create paramaters
     #get base shape
@@ -272,7 +407,7 @@ def CreatePlaneBtnCmd():
     NumberOfDivision = mc.intSliderGrp(divisitonintField, q = True, v =True)
     createCtroller = mc.checkBox(createCtrlCB, q = True, v = True)
     controllerType = mc.radioCollection("ControllerSelectionID", q = True, sl = True)
-    NameBase = mc.textField("ControllerTextFieldID", q = True, tx = True)
+    NameBase = mc.textFieldGrp("ControllerTextFieldID", q = True, tx = True)
     specifyBaseShape = mc.checkBox(SpecifyBaseShapeCB, q = True, v = True)
     BaseShapeFiledText = mc.textField("BasicShapeTextFieldID", q = True, tx = True)
     
@@ -294,9 +429,6 @@ def CreatePlaneBtnCmd():
             BaseShape = item
         else:
             TargetShape = item
-    
-    print "base shape is: " + BaseShape
-    print "Target Shape is: " + TargetShape
     if specifyBaseShape and BaseShape == "":
         BaseShape = BaseShapeFiledText
     if BaseShape == "":
@@ -439,11 +571,7 @@ def MakeDivistionPlan(boundingBox, basePosition, targetPosition, targetShape, ba
     TargetBoundingBoxYPovit = ((boundingBox[2] + boundingBox[3])/2.0) + offset.y
     TargetBoundingBoxZPovit = ((boundingBox[4] + boundingBox[5])/2.0) + offset.z
     TargetBoundingBoxXPovit = ((boundingBox[0] + boundingBox[1])/2.0) + offset.x
-    
-    planGrpName = targetShape + "_DivistionPlaneGrp"
-    #create an empty grp
-    mc.group(n = planGrpName, em = True)
-    
+     
     #start making planes
     for i in range(0, numOfDivision):  
         #create the plane
@@ -467,12 +595,6 @@ def MakeDivistionPlan(boundingBox, basePosition, targetPosition, targetShape, ba
             mc.setAttr(PlaneName + ".baseShape", baseShape, type = "string")
             
         Planes.append(PlaneName)
-        mc.parent(PlaneName, planGrpName)
-    
-    #Center poivots
-    mc.xform(planGrpName,cp = True)
-    #parent to targetShape
-    #mc.parent(planGrpName, targetShape)
     return Planes
     
     
